d93a9e68c3ba708f9ce25b87037f20ec21833ee1
Jenslee Dsouza <dsouzajenslee@gmail.com>
2023-02-13 16:26:05 +0530

    Added function to generate OTP

---
+++ firstproject/myapp/otp.py
@@ -0,0 +1,20 @@
+import math
+import random
+
+
+def generateOTP():
+
+    # Declare a digits variable
+    # which stores all digits
+    digits = "0123456789"
+    OTP = ""
+
+    # length of password can be changed
+    # by changing value in range
+    for i in range(6):
+        OTP += digits[math.floor(random.random() * 10)]
+
+    return OTP
+
+
+print(generateOTP())
