a2ab87665697661049f60e3491929dc5ee60003b
Jenslee Dsouza <dsouzajenslee@gmail.com>
2023-02-13 16:06:54 +0530

    feat:Added logic to read dotenv files

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -9,6 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(find_dotenv())
 
 from pathlib import Path
 
